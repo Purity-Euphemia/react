95efe1c68f160ac32fac9a290871fac141549644
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-10-22 11:12:34 +0100

    first commit

--- e-Commerce/src/auth/register/Register.jsx
+++ e-Commerce/src/auth/register/Register.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link } from "react-router";
+import styles from "./Register.module.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="firstname">FirstName:</label>
+          <input type="text" placeholder="firstName"/>
+        </div>
+
+        <div>
+          <label htmlFor="lastname">LastName:</label>
+          <input type="text" placeholder="lastName"/>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" placeholder="email"/>
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" placeholder="Password"/>
+        </div>
+      </form>
+      
+      <div className={styles.linkToLogin}>
+        <p> Already have an account?</p> 
+        <Link to="/login">Login</Link>
+      </div>
+    </div>
   )
 }
 
